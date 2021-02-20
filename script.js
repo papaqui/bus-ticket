8c53ecd67dfd77d3b923af4df82aa00937d8f5ab
Fernando Papaqui <hola@rivaslevi.mx>
2021-02-20 10:19:57 -0600

    create select-trip section

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 'use strict';
 
+const typeOfTripSingle = document.getElementById('single-trip');
+const typeOfTripRound = document.getElementById('round-trip');
+const returnDate = document.querySelector('.origin-hidden');
+const booking = document.getElementById('booking');
+const busSeats = document.getElementById('bus-seats-container');
 const seat = document.querySelectorAll('.seat');
 const bus = document.querySelector('.bus');
+const cancelTimer = document.getElementById('cancel-timer');
+
+var timer;
+
+// Single or round trip
+typeOfTripRound.addEventListener('click', () => {
+  returnDate.classList.add('show-flex');
+});
+typeOfTripSingle.addEventListener('click', () => {
+  returnDate.classList.remove('show-flex');
+});
+
+// Booking btn makes bus seat selection display
+booking.addEventListener('click', () => {
+  busSeats.classList.add('show');
+});
+
+function sendTacos() {
+  alert('QUIERO CARNE!');
+}
 
 bus.addEventListener('click', (e) => {
   if (
@@ -9,5 +34,12 @@ bus.addEventListener('click', (e) => {
     !e.target.classList.contains('occupied')
   ) {
     e.target.classList.toggle('selected');
+    timer = setTimeout(sendTacos, 3000);
   }
 });
+
+function cancelTimeout() {
+  clearTimeout(timer);
+}
+
+cancelTimer.addEventListener('click', cancelTimeout);
